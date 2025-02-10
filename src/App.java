7b43a3f842331f9b559cb91d09edb419947c5f61
Mohammed-Tahmid-Uddin <mohammed.uddin.20@city.ac.uk>
2025-02-10 18:34:52 +0000

    Creating the Main app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JFrame Window=new JFrame();
+
+        Window.setVisible(true);
+        Window.setExtendedState(JFrame.MAXIMIZED_BOTH);  
+        Window.setTitle("Lancaster's Music Hall App");
+        Window.getContentPane().setBackground(Color.WHITE);
+
+        Window.setResizable(false);
+
     }
 }
