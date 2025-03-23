ebc2cf20c1d985e46048364f4d62f0aa2bb11770
Mohammed-Tahmid-Uddin <mohammed.uddin.20@city.ac.uk>
2025-03-23 16:23:27 +0000

    Made small Hall
    Because of how i coded the Main hall, was able to just take out the balcony and resize the stalls. changed to use GridBagLayout rather than GridLayout because of the sizing issue with the buttons. GridBagLaout, gives me control

--- src/App.java
+++ src/App.java
@@ -13,7 +13,8 @@ public class App {
 
         Window.setResizable(false);
 
-        SeatBooking seatBooking=new SeatBooking(Window);
+        //SeatBooking seatBooking=new SeatBooking(Window);
+        SmallHall hall=new SmallHall(Window);
 
     }
 }
